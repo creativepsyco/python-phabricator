efaeeb07f40695b8fd34f403bf4f7fc6f1cd8518
Mohit Kanwal <kanwalm@garena.com>
2014-03-09 17:56:03 +0800

    Adding Session.py to get the current Session

---
+++ config/settings_sample.py
@@ -0,0 +1,18 @@
+# Original documentation: http://justin.harmonize.fm/development/2013/06/29/authenticating-with-phabricator.html
+# Constants to be filled in by the reader
+
+"""
+This file privdes configuration options.
+After modifying it with the necessary values
+Rename this as settings.py Or copy over and then rename
+"""
+
+# The Certificate of your phabricator installation that can be found
+# under settings
+CERT = 'YOUR_CERTIFICATE_HERE'
+
+# The authorized user on behalf of which to make the call
+USER = 'USER_NAME'
+
+# The phabricator URL without the PHAB
+PHAB = 'PHABRICATOR_URL'
